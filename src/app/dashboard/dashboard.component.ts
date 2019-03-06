491cd5d2328523cc164ef75bb1720e9dfc6dd844
bella812 <bella.munoz812@gmail.com>
2019-03-06 12:17:35 -0500

    section one is done still needs an image

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  Designation: string;
+  Username: string;
+  NoOfTeamMembers: number;
+  TotalCostOfAllProjects: number;
+  PendingTasks: number;
+  UpComingProjects: number;
+  ProjectCost: number;
 
-  constructor() { }
+  CurrentExpenditure: number;
+  AvailableFunds: number;
 
   ngOnInit() {
+    this.Designation = "Team Leader";
+    this.Username = "John Smith";
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 976788;
+    this.AvailableFunds = 52536;
   }
 
 }
